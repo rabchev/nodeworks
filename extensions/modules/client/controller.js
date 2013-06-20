3cf22afa5c3fe544409364ad5d5c513e75cc5924
Boyan Rabchev <boyan@rabchev.com>
2013-06-20 17:23:04 +0300

    Update brackets-src

--- extensions/modules/client/controller.js
+++ extensions/modules/client/controller.js
@@ -176,7 +176,6 @@ define(function (require, exports, module) {
     function showNPM() {
         setCurrRepo("npm");
         
-        
     }
     
     module.exports = exports = function (edtr) {
